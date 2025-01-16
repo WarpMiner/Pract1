e1bab7b73c11c346853245f4bdf3433dd3ceb461
WarpMiner <163392665+WarpMiner@users.noreply.github.com>
2025-01-16 19:31:36 +0700

    Update includes.h

--- include/includes.h
+++ include/includes.h
@@ -1,7 +1,12 @@
-#pragma once
+#ifndef includes_h
+#define includes_h
 
 using namespace std;
 
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <filesystem>
+#include <functional>
+
+#endif // includes_h
