98d14bd2339e0e3b24093febcbc694c99fea4f8e
WarpMiner <163392665+WarpMiner@users.noreply.github.com>
2025-01-16 19:33:14 +0700

    Update list.cpp

--- src/list.cpp
+++ src/list.cpp
@@ -1,12 +1,29 @@
 #include "../include/list.h"
 
-template<typename T>
-Node<T>::Node(T value) : data(value), next(nullptr) {}
+template <typename T>
+Node<T>::Node(T value) {
+    data = value;
+    next = nullptr;
+} 
 
-template<typename T>
-SinglyLinkedList<T>::SinglyLinkedList() : head(nullptr) {}
+template <typename T>
+SinglyLinkedList<T>::SinglyLinkedList() {
+    head = nullptr; //Если список не содержит элементов, то присваиваем нулевое значение
+}
 
-template<typename T>
+template <typename T>
+SinglyLinkedList<T>::~SinglyLinkedList() {
+    while(!isEmpty()) {
+        popFront(); //Вызывает список, после которого очищает память
+    }
+}
+
+template <typename T>
+bool SinglyLinkedList<T>::isEmpty() const {
+    return elementCount == 0;
+}
+
+template <typename T>
 void SinglyLinkedList<T>::print() {
     Node<T>* current = head;
     while (current) {
@@ -16,106 +33,96 @@ void SinglyLinkedList<T>::print() {
     cout << endl;
 }
 
-template<typename T>
-void SinglyLinkedList<T>::push_front(T value) {
-    Node<T>* newNode = new Node<T>(value);
-    newNode->next = head;
+template <typename T>
+void SinglyLinkedList<T>::pushFront(T value) { 
+    Node<T>* newNode = new Node(value);
+    newNode->next = head; //Следующий узел, становится текущей головой списка
     head = newNode;
-    size++;
+    elementCount++;
 }
 
-template<typename T>
-void SinglyLinkedList<T>::push_back(T value) {
-    Node<T>* newNode = new Node<T>(value);
-    if (head == nullptr) {
+template <typename T>
+void SinglyLinkedList<T>::pushBack(T value) {
+    Node<T>* newNode = new Node(value);
+    if(head == nullptr) { //Условие если список пуст
         head = newNode;
     } else {
         Node<T>* current = head;
-        while (current->next) {
-            current = current->next;
+        while(current->next != nullptr) {
+            current = current->next; //Присваиваем current следующее значение
         }
         current->next = newNode;
     }
-    size++;
+    elementCount++;
 }
 
-template<typename T>
-void SinglyLinkedList<T>::pop_front() {
-    if (head == nullptr) return;
-    Node<T>* temp = head;
-    head = head->next;
-    delete temp;
-    size--;
+template <typename T>
+void SinglyLinkedList<T>::popFront() {
+    if(head == nullptr) return;
+    Node<T>* nextHead = head->next; //Создание временной переменной, для сохранения
+    delete head;
+    head = nextHead;
+    elementCount--;
 }
 
-template<typename T>
-void SinglyLinkedList<T>::pop_back() {
-    if (head == nullptr) return;
-    if (!head->next) { // если только 1 элемент
+template <typename T>
+void SinglyLinkedList<T>::popBack() {
+    if (head == nullptr) return; // Если список пуст
+    if (head->next == nullptr) { // Если один элемент
         delete head;
-        head = nullptr;
-        return;
-    }
-    Node<T>* current = head;
-    while (current->next && current->next->next) {
-        current = current->next;
-    }
-    delete current->next; // Удаляем последний элемент
-    current->next = nullptr; // Обнуляем указатель
-    size--;
-}
-
-template<typename T>
-void SinglyLinkedList<T>::remove(T value) {
-    if (head == nullptr) return;
-    if (head->data == value) {
-        pop_front();
-        return;
-    }
-    Node<T>* current = head;
-    while (current->next) {
-        if (current->next->data == value) {
-            Node<T>* temp = current->next;
-            current->next = current->next->next;
-            delete temp;
-            return;
-        }
-        current = current->next;
-    }
-    size--;
-}
-
-template<typename T>
-void SinglyLinkedList<T>::replace(int index, T newValue) {
-        if (index < 0 || index >= size) {
-            cout << "Index out of bounds." << endl;
-            return;
-        }
-
+        head = nullptr; // Исправлено
+    } else {
         Node<T>* current = head;
-        for (int i = 0; i < index; i++) {
+        while (current->next->next != nullptr) {
             current = current->next;
         }
-        current->data = newValue;
-    }
-
-template<typename T>
-int SinglyLinkedList<T>::getindex(T value) {
-    Node<T>* current = head;
-    int index = 0;
-    while (current) {
-        if (current->data == value) {
-            return index; // Элемент найден, возвращаем индекс
+            delete current->next; // Удаляем последний элемент
+            current->next = nullptr; // Обнуляем указатель на следующий элемент
         }
-        current = current->next;
-        index++;
-    }
-    return -1; // Если элемент не найден, возвращаем -1
+    elementCount--;
 }
 
-template<typename T>
-T SinglyLinkedList<T>::getvalue(int index) {
-    if (index < 0 || index >= size) {
+template <typename T>
+void SinglyLinkedList<T>::removeAt(T value) {
+    if (isEmpty()) return;
+    if (head->data == value) {
+        popFront();
+        return;
+    }
+    Node<T>* current = head;
+    while (current->next != nullptr && current->next->data != value) {
+        current = current->next;
+    }
+    if (current->next != nullptr) {
+        Node<T>* nodeToDelete = current->next;
+        current->next = nodeToDelete->next;
+        delete nodeToDelete;
+        elementCount--;
+    }
+}
+
+template <typename T>
+bool SinglyLinkedList<T>::find(T value) {
+    Node<T>* current = head;
+    while (current != nullptr) {
+        if (current->data == value) {
+            return true;
+        }
+        current = current->next;
+    }
+    return false;
+}
+
+template <typename T>
+void SinglyLinkedList<T>::clearSList() {
+    while (!isEmpty()) {
+        popFront();
+    }
+}
+
+template <typename T>
+T SinglyLinkedList<T>::getElementAt(int index) const {
+    if (index < 0 || index >= elementCount) {
         throw out_of_range("Index out of range");
     }
 
@@ -127,123 +134,40 @@ T SinglyLinkedList<T>::getvalue(int index) {
     return current->data;
 }
 
-template<typename T>
-SinglyLinkedList<T>::~SinglyLinkedList() {
-    while (head) {
-        pop_front();
-    }
-}
-
-
-
-template<typename T>
-Node2<T>::Node2(T value) : data(value), next(nullptr), prev(nullptr) {}
-
-template<typename T>
-DoublyLinkedList<T>::DoublyLinkedList() : head(nullptr), tail(nullptr) {}
-
-template<typename T>
-void DoublyLinkedList<T>::print() { 
-    Node2<T>* current = head;
-    while (current) {
-        cout << current->data << " ";
-        current = current->next;
-    }
-    cout << endl;
-}
-
-template<typename T>
-void DoublyLinkedList<T>::push_front(T value) {
-    Node2<T>* newNode = new Node<T>(value);
-    if (head == nullptr) {
-        head = tail = newNode;
-    } else {
-        newNode->next = head;
-        head->prev = newNode;
-        head = newNode;
-    }
-    size++;
-}
-
-template<typename T>
-void DoublyLinkedList<T>::push_back(T value) {
-    Node2<T>* newNode = new Node<T>(value);
-    if (tail == nullptr) {
-        head = tail = newNode;
-    } else {
-        newNode->prev = tail;
-        tail->next = newNode;
-        tail = newNode;
-    }
-    size++;
-}
-
-template<typename T>
-void DoublyLinkedList<T>::pop_front() {
-    if (head == nullptr) return;
-    Node2<T>* temp = head;
-    head = head->next;
-    if (head != nullptr) {
-        head->prev = nullptr;
-    } else {
-        tail = nullptr; // Список стал пустым
-    }
-    delete temp;
-    size--;
-}
-
-template<typename T>
-void DoublyLinkedList<T>::pop_back() { 
-    if (tail == nullptr) return; // Список пуст
-    Node2<T>* temp = tail;
-    tail = tail->prev;
-    if (tail != nullptr) {
-        tail->next = nullptr;
-    } else {
-        head = nullptr; // Список стал пустым
-    }
-    delete temp;
-    size--;
-}
-
-template<typename T>
-void DoublyLinkedList<T>::remove(T value) {
-    Node2<T>* current = head;
+template <typename T>
+int SinglyLinkedList<T>::getIndex(T value) {
+    Node<T>* current = head;
+    int index = 0;
     while (current) {
         if (current->data == value) {
-            if (current->prev) {
-                current->prev->next = current->next;
-            } else {
-                head = current->next; // Удаляем голову
-            }
-            if (current->next) {
-                current->next->prev = current->prev;
-            } else {
-                tail = current->prev; // Удаляем хвост
-            }
-            delete current;
-            return; // Выход после удаления первого найденного элемента
+            return index; // Возвращаем индекс, если значение найдено
         }
         current = current->next;
+        index++;
     }
-    size--;
+    return -1; // Возвращаем -1, если значение не найдено
 }
 
 template<typename T>
-bool DoublyLinkedList<T>::find(T value) {
-    Node2<T>* current = head;
-    while (current) {
-        if (current->data == value) {
-            return true; // Элемент найден
-        }
+void SinglyLinkedList<T>::replace(int index, T newValue) {
+    if (index < 0 || index >= size()) {
+        cout << "Index out of bounds." << endl;
+        return;
+    }
+
+    Node<T>* current = head;
+    for (int i = 0; i < index; i++) {
         current = current->next;
     }
-    return false; // Элемент не найден
+    current->data = newValue;
 }
 
-template<typename T>
-DoublyLinkedList<T>::~DoublyLinkedList() {
-    while (head) {
-        pop_front();
-    }
+template <typename T>
+Node<T>* SinglyLinkedList<T>::getHead() const {
+    return head;
+}
+
+template <typename T>
+size_t SinglyLinkedList<T>::size() {
+    return elementCount;
 }
